38161700af2ad11dc067e6681f51e5f616ad52e8
longwatermelon <longwatermelon1@gmail.com>
2021-07-28 21:22:39 -0700

    lexer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let contents = fs::read_to_string(args[1].as_str()).expect("Couldn't open file");
 
-    let lexer = lexer::init_lexer(String::from(contents.as_str()));
-    println!("{}", lexer.contents);
+    let mut lexer = lexer::init_lexer(String::from(contents.as_str()));
+    let mut dummy = lexer::lexer_collect_next_token(&mut lexer);
+
+    println!("started");
+
+    loop {
+        if dummy.token_type == lexer::TokenType::Eof {
+            break;
+        }
+
+        println!("type: {}, value: {}", dummy.token_type as u32, dummy.value.as_str());
+        dummy = lexer::lexer_collect_next_token(&mut lexer);
+    }
+
+    println!("Finished");
 }
